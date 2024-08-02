29eba790b532179931b64bcfe5142c03239bb904
eren5854 <ihsanerendelibas@hotmail.com>
2024-08-03 01:49:22 +0300

    Add project files.

---
+++ AlternativeEngineerBlogServer.Domain/Abstractions/Entity.cs
@@ -0,0 +1,34 @@
+﻿namespace AlternativeEngineerBlogServer.Domain.Abstractions;
+public abstract class Entity
+{
+    protected Entity() 
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string UpdatedBy { get; set; }
+    public DateTime UpdatedDate { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return Id == entity.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
